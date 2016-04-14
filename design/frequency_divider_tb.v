f982a6a3f12e059955d55f478c776d9a6d69f9af
gang ji <jigangujs@gmail.com>
2016-04-14 22:48:49 +0800

    Added files via upload

---
+++ design/frequency_divider_tb.v
@@ -0,0 +1,26 @@
+`timescale		1ns/1ns 
+
+module		frequency_divider_tb;
+
+	reg		clk,rst_n;
+	
+	wire	clk_out;
+	
+initial
+	begin
+		clk		=	0;
+		rst_n	=	0;
+		#200
+		rst_n	=	1;
+	end
+
+always	#10		clk	=	~clk;
+
+frequency_divider		#(	.N(5)	)			//带参数例化
+					frequency_divider_inst(
+	.clk					(clk),
+	.rst_n					(rst_n),
+	.clk_out				(clk_out)
+);
+
+endmodule
